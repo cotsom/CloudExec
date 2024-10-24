71201f0f2059826156c12b43fccf039d6d9c2f0d
cotsom <gerfy.17@mail.ru>
2024-10-24 21:00:52 +0300

    add grafana

---
+++ clx/mods/grafana/grafana.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	modules "clx/mods/test/modules"
+	utils "clx/utils"
+	"fmt"
+	"os"
+	"path/filepath"
+	"sync"
+)
+
+// mode type for plugin's symbol
+type mode string
+
+type Module interface {
+	RunModule(arg string)
+}
+
+var registeredModules = map[string]Module{
+	"module1": modules.Module1{},
+	// Add another modules here
+}
+
+func checkGrafana(targets string) {
+
+}
+
+func (m mode) Run(args []string) {
+	if len(args) < 1 {
+		fmt.Println("Enter host or subnetwork")
+		return
+	}
+
+	var targets = utils.ParseTargets(args[0])
+	fmt.Println(targets)
+
+	moduleName, err := utils.GetParam(args, "-M")
+	if err != nil {
+		fmt.Println("You have to chose module here")
+		os.Exit(0)
+	}
+
+	//Mode logic
+	if moduleName == "" {
+		var wg sync.WaitGroup
+		sem := make(chan struct{}, 256)
+		for _, target := range targets {
+			wg.Add(1)
+			sem <- struct{}{}
+			go checkGrafana(target.String())
+		}
+	} else {
+		rootPath, err := os.Executable()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		modules := utils.GetModulesName(fmt.Sprintf("%s/mods/test/modules", filepath.Dir(rootPath)))
+
+		if !utils.Contains(modules, moduleName) {
+			fmt.Printf("there is no %s module, chose ones from list \n%s\n", moduleName, modules)
+			return
+		}
+
+		if module, exists := registeredModules[moduleName]; exists {
+			module.RunModule("lol")
+		} else {
+			fmt.Printf("Module %s not found. Available modules: %v\n", moduleName, modules)
+			os.Exit(1)
+		}
+	}
+
+}
+
+// exporting symbol "Mode"
+var Mode mode
