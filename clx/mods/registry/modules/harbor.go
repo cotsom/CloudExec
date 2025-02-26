63b75fcb128589a5432a834e24e43a6f48c94ce9
cotsom <gerfy.17@mail.ru>
2025-02-26 19:19:50 +0300

    upd harbor module

--- clx/mods/registry/modules/harbor.go
+++ clx/mods/registry/modules/harbor.go
@@ -23,9 +23,18 @@ type Repository struct {
 	RepositoryName string `json:"repository_name"`
 }
 
+type Artifact struct {
+	AdditionLinks struct {
+		BuildHistory struct {
+			Href string `json:"href"`
+		} `json:"build_history"`
+	} `json:"addition_links"`
+}
+
 func (m Harbor) RunModule(target string, flags map[string]string, scheme string) {
 	port := flags["port"]
 	var images Harbor
+	var artifacts []Artifact
 
 	client := http.Client{
 		Timeout: 10 * time.Second,
@@ -71,6 +80,55 @@ func (m Harbor) RunModule(target string, flags map[string]string, scheme string)
 	}
 
 	for _, image := range images.Repository {
-		utils.Colorize(utils.ColorYellow, fmt.Sprintf("[+] %s - %s (Artifacts: %d, Pulls: %d)\n", target, image.RepositoryName, image.ArtifactCount, image.PullCount))
+		utils.Colorize(utils.ColorGreen, fmt.Sprintf("[+] %s - %s (Artifacts: %d, Pulls: %d)\n", target, image.RepositoryName, image.ArtifactCount, image.PullCount))
+		repoNameSplit := strings.SplitN(image.RepositoryName, "/", 2)
+		// var layers map[string]interface{}
+
+		url := fmt.Sprintf("%s://%s:%s@%s:%s/api/v2.0/projects/%s/repositories/%s/artifacts?with_tag=false&with_scan_overview=true&with_label=true&with_accessory=false&page_size=15&page=1", scheme, flags["user"], flags["password"], target, port, repoNameSplit[0], strings.ReplaceAll(repoNameSplit[1], "/", "%252F"))
+		response, err = utils.HttpRequest(url, http.MethodGet, []byte(""), client)
+		if err != nil {
+			return
+		}
+		respBody, err = ioutil.ReadAll(response.Body)
+		defer response.Body.Close()
+		if err != nil {
+			fmt.Printf("client: could not read response body: %s\n", err)
+		}
+
+		err := json.Unmarshal(respBody, &artifacts)
+		if err != nil {
+			fmt.Println("Error unmarshalling JSON:", err)
+			return
+		}
+
+		for _, artifact := range artifacts {
+			url := fmt.Sprintf("%s://%s:%s@%s:%s/%s", scheme, flags["user"], flags["password"], target, port, artifact.AdditionLinks.BuildHistory.Href)
+			response, err = utils.HttpRequest(url, http.MethodGet, []byte(""), client)
+			if err != nil {
+				return
+			}
+			respBody, err = ioutil.ReadAll(response.Body)
+			defer response.Body.Close()
+			if err != nil {
+				fmt.Printf("client: could not read response body: %s\n", err)
+			}
+
+			var data interface{}
+			err = json.Unmarshal(respBody, &data)
+			if err != nil {
+				fmt.Println("Ошибка при декодировании JSON:", err)
+				// fmt.Println(string(respBody))
+				// return
+			}
+
+			prettyJSON, err := json.MarshalIndent(data, "", "  ")
+			if err != nil {
+				fmt.Println("Ошибка при форматировании JSON:", err)
+				return
+			}
+
+			fmt.Println(string(prettyJSON))
+			utils.Colorize(utils.ColorYellow, fmt.Sprintf("%s\n", string(prettyJSON)))
+		}
 	}
 }
