57c630e0cc9ad3f7278d9ba5826e31b05de8bbf8
SS4ar <s4ar.gofunprod@gmail.com>
2025-07-02 15:44:55 +0300

    fix absence urlencode in url scheme creds registry modules

--- internal/modules/registry/images.go
+++ internal/modules/registry/images.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"time"
 
 	"github.com/cotsom/CloudExec/internal/utils"
@@ -24,8 +25,9 @@ func (m Images) RunModule(target string, flags map[string]string, scheme string)
 			return http.ErrUseLastResponse
 		},
 	}
+	creds := fmt.Sprintf("%s:%s", flags["user"], url.QueryEscape(flags["password"]))
 
-	url := fmt.Sprintf("%s://%s:%s@%s:%s/v2/_catalog", scheme, flags["user"], flags["password"], target, port)
+	url := fmt.Sprintf("%s://%s@%s:%s/v2/_catalog", scheme, creds, target, port)
 	response, err := utils.HttpRequest(url, http.MethodGet, []byte(""), client)
 	if err != nil {
 		return
